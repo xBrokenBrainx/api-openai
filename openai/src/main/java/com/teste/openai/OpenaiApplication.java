4da2f366325438ba735b0922ddb685349234bec5
Gustavo Matheus Nunes <56331592+nwrn@users.noreply.github.com>
2023-04-13 20:24:44 -0300

    Todos parâmetros passado para variáveis
    Adicionado também os parâmetros: MAX_TOKENS e TEMPERATURE

--- openai/src/main/java/com/teste/openai/OpenaiApplication.java
+++ openai/src/main/java/com/teste/openai/OpenaiApplication.java
@@ -12,29 +12,53 @@ import java.io.InputStreamReader;
 public class OpenaiApplication {
 
     private static String KEY = "sk-";
+    private static String PROMPT = "O que e o chat GPT?";
+    private static long MAX_TOKENS = 100;
+    private static float TEMPERATURE = 1;
+    private static String MODEL = "text-davinci-003";
 
-    public static void main(String[] args) throws Exception{
+
+    public static void main(String[] args){
 
         try{
             DefaultHttpClient client = new DefaultHttpClient();
             HttpPost post = new HttpPost
                     ("https://api.openai.com/v1/completions");
 
-            StringEntity entity = new StringEntity("{\"model\" : \"text-davinci-003\",\"prompt\" : \"" +
-                    "O que e a nova rota da seda\"}");
+            StringEntity entity = new StringEntity
+                    ("{" +
+                            "\"model\" : " +
+                            "\"" +
+                            MODEL +
+                            "\"," +
+                            "\"prompt\" : \"" +
+                            PROMPT +
+                            "\"," +
+                            "\"max_tokens\" : " +
+                            MAX_TOKENS +
+                            "," +
+                            "\"temperature\" : " +
+                            TEMPERATURE +
+                            "}");
+
             entity.setContentType("application/json");
             post.setHeader("Content-Type","application/json");
             post.setHeader("Authorization","Bearer "+KEY);
             post.setEntity(entity);
+
             HttpResponse response = client.execute(post);
+
             if(response.getStatusLine().getStatusCode() != 201)
                 System.out.println("HTTP Error: "+response.getStatusLine().getStatusCode());
+
             BufferedReader reader = new BufferedReader(
                     new InputStreamReader(response.getEntity().getContent()));
+
             String output;
             System.out.println("\n\nGPT Resposta: \n");
             while ( (output = reader.readLine()) != null)
                 System.out.println(output);
+
             client.getConnectionManager().shutdown();
 
         }catch (Exception exception){System.out.println( exception.getMessage() );}
